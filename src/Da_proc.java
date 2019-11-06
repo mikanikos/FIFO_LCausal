eb123dcaa53e6458dfb9a1c4a194fe4b5f9c735e
cristianbotezatu <cristianbotezatu@eduroam-2-107.epfl.ch>
2019-11-06 17:54:26 +0100

    Minnor updates

--- src/Da_proc.java
+++ src/Da_proc.java
@@ -61,6 +61,7 @@ public class Da_proc {
         int myPort = processes.get(id).getPort();
         new Thread(new Receiver(myPort)).start();
         new Thread(new URBroadcast()).start();
+        new Thread(new PerfectLink()).start();
 
         while(SignalHandlerUtility.wait_for_start) {
             Thread.sleep(1000);
